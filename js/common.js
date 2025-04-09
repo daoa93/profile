c0269bef31dd66850e59fddd4a7f0b5f9f352f7e
daoa93 <daoa93@naver.com>
2025-04-09 16:19:56 +0900

    feat: 회원가입 폼 추가

--- js/common.js
+++ js/common.js
@@ -1,5 +1,8 @@
-// 메뉴 버튼 토글
-const btnToggle = document.querySelector('.btnToggle');
-btnToggle.addEventListener('click', () => {
+//메뉴 버튼 토글
+const btnToggle = document.querySelector(".btnToggle");
+const mobileMenu = document.querySelector(".mobileMenu");
+
+btnToggle.addEventListener("click", () => {
   btnToggle.classList.toggle("on");
+  mobileMenu.classList.toggle("on");
 });
