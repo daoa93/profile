29caddc97671a32eb4f268682118304f612f7ae2
daoa93 <daoa93@naver.com>
2025-04-11 11:21:31 +0900

    feat: my works 메뉴에 youtube 연동 추가 완료

--- js/youtube.js
+++ js/youtube.js
@@ -1,24 +1,72 @@
 const api_key = "AIzaSyAg7wgMMExG7Wi1lF4-VAqR9GkV6uNSZYE";
 const pid = "PLqsTrDmjWMv6aPOL3m5oDTY6ot24lqxqe";
 const base_url = "https://www.googleapis.com/youtube/v3/playlistItems";
-const num = 2;
+const num = 3;
 const req_url = `${base_url}?part=snippet&playlistId=${pid}&key=${api_key}&maxResults=${num}`;
+const frame = document.querySelector(".frame");
 
-//요청 url을 인자로 전달해서 서버에 데이터 요청후 가져오기
 fetch(req_url)
   .then((data) => {
-    //모든 데이터가 받아지면 data라는 매개변수로 전달됨
-    //js로 가공가능한 json형태로 변환시킴
     return data.json();
   })
   .then((json) => {
-    //json데이터 변환이 완료되면 최종 데이터 콘솔로 확인
     const youtubeData = json.items;
+
+    let tags = "";
 
     youtubeData.forEach((data, index) => {
       console.log(data);
-      console.log(data.snippet.title);
-      console.log(data.snippet.publishedAt);
+
+      let tit = "";
+      let desc = "";
+
+      if (data.snippet.title.length > 35) {
+        tit = data.snippet.title.substring(0, 35) + "...";
+      } else {
+        tit = data.snippet.title;
+      }
+
+      data.snippet.description.length > 120
+        ? (desc = data.snippet.description.substring(0, 120) + "...")
+        : (desc = data.snippet.description);
+
+      tags += `
+        <article>
+          <h2>${tit}</h2>
+          <figure class='pic' data-vid=${data.snippet.resourceId.videoId}>
+            <img src=${data.snippet.thumbnails.high.url} />
+          </figure>
+          <p>${desc}</p>
+          <span>${data.snippet.publishedAt
+          .split("T")[0]
+          .split("-")
+          .join(".")}</span>
+        </article>      
+      `;
+    });
+
+    frame.innerHTML = tags;
+
+    const popup = document.querySelector(".popup");
+    const popupCon = popup.querySelector(".con");
+    const pics = document.querySelectorAll(".pic");
+    const closePop = document.querySelector(".closePop");
+
+    pics.forEach((data, index) => {
+      data.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        console.dir(data);
+        const vidId = data.getAttribute("data-vid");
+        console.log(vidId);
+
+        popup.style.display = "block";
+        popupCon.innerHTML = `<iframe src="http://www.youtube.com/embed/${vidId}"></iframe>`;
+      });
+    });
+
+    closePop.addEventListener("click", (e) => {
+      e.preventDefault();
+      popup.style.display = "none";
     });
   });
-
